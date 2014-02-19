e423d2d619bab20a12cef388ce1b8f0e2a39ec2b
Bartosz Banachewicz <bartosz.banachewicz@intel.com>
2014-02-19 14:02:34 +0100

    Added another dumb sample, this time with four triangles

---
+++ 2d/sample2.hs
@@ -0,0 +1,31 @@
+import qualified Graphics.Rendering.OpenGL as GL
+import Glisha2D
+
+import Control.Monad.Trans
+import Control.Monad.Trans.State.Lazy
+
+type SampleState = [Instance]
+
+sampleLoad :: LoadFn SampleState
+sampleLoad = do
+    pipeline <- createPipeline "shader.vert" "shader.frag"
+    let vertexData = [
+            -0.6, -0.4,
+            0.6, -0.4,
+            0,    0.6
+            ]
+    mesh <- fromVertArray vertexData
+
+    let inst = [createInst (-0.5, -0.5),
+                createInst (-0.5,  0.5),
+                createInst (0.5,  -0.5),
+                createInst (0.5,   0.5)]
+            where createInst (x,y) = Instance mesh pipeline (GL.Vertex2 x y :: GL.Vertex2 GL.GLfloat)
+   
+    return inst
+
+sampleDraw :: DrawFn SampleState
+sampleDraw = get >>= liftIO . (mapM_ draw)
+
+main = runGlisha sampleLoad sampleDraw
+
